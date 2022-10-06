2cee35f456430b55d8742e05d17f81a40080c1f9
Hamza <diagonalge@gmail.com>
2022-10-07 01:18:58 +0500

    commited

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.jan),
-    path("february", views.feb)
+    path("<int:month>", views.monthly_challenge_num),
+    path("<str:month>", views.monthly_challenge, name = "month-challenge")
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,10 +1,39 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound,HttpResponseRedirect
+from django.urls import reverse
+
+import challenges
 
 # Create your views here.
 
-def jan(request):
-    return HttpResponse("Eat no meat for the entire month!")
+challenges = {
+    "january": "Dont eat meat for 30 days",
+    "february": "Pray 5 times a day for 30 days",
+    "march": "Walk for 30 minutes everyday",
+    "april": "ast for 1 day after every 2 days",
+    "may": "Run for 1 mile a day",
+    "june": "Dont swear for 30 days",
+    "july": "Give $5 to charity everyday",
+    "august": "Don't lie at all for 30 days",
+    "september": "Walk for 1 hour everyday",
+    "october": "Fast for 15 days in a month",
+    "november": "Pray 5 times with congregation",
+    "december": "Run for 2 miles everyday"
+}
 
-def feb(request):
-    return HttpResponse("Walk for 30 minutes everyday.")
+def monthly_challenge_num(request, month):
+    months = list(challenges.keys())
+    try:
+        redirect_month = months[month-1]
+        redirect_path = reverse("month-challenge", args = [redirect_month])
+        return HttpResponseRedirect(redirect_path)
+    except:
+        return HttpResponseNotFound("Invalid month")
+
+def monthly_challenge(request, month):
+    challenge_text = ""
+    try:
+        challenge_text = challenges[month]
+    except:
+        return HttpResponseNotFound("Invalid Month")
+    return HttpResponse(challenge_text)
